0432fe4cdf5af5681aa5f4f8b630d321ba513686
equationslayer12 <roy0708usa@gmail.com>
2023-08-13 23:20:41 +0300

    added project architecture

--- birthday_slayer.py
+++ birthday_slayer.py
@@ -1,8 +1,14 @@
+from videoMaker import videoMaker
+from thumbnailMaker import thumbnailMaker
 
 
 def main():
-    ...
+    vid_maker = videoMaker.VideoMaker()
+    thumb_maker = thumbnailMaker.ThumbnailMaker()
 
+    for video_name in ["daniel.mp4", "equations.mp4"]:
+        vid_maker.create_video(video_name)
+        thumb_maker.create_thumbnail(video_name)
 
 if __name__ == "__main__":
     main()
