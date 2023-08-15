7bf2059ae5a6de88cd8f03e84654ba9beb714e5f
iKappyyyy <specialtimewithdaniel@gmail.com>
2023-08-15 04:13:09 +0300

    Added some code to add the end clip with the name, but it doesn't work since ImageMagick is needed to use it and can't bother right now.

--- birthday_slayer.py
+++ birthday_slayer.py
@@ -1,5 +1,12 @@
 from videoMaker import videoMaker
 from thumbnailMaker import thumbnailMaker
+from os import listdir
+
+
+def get_videos_in_folder(folder_path):
+    video_paths = [f"{folder_path}/{video_name}" for video_name in listdir(folder_path)]
+
+    return video_paths
 
 
 def main():
@@ -9,6 +16,7 @@ def main():
     for video_name in ["daniel.mp4", "equations.mp4"]:
         vid_maker.create_video(video_name)
         thumb_maker.create_thumbnail(video_name)
+
 
 if __name__ == "__main__":
     main()
--- videoMaker/videoMaker.py
+++ videoMaker/videoMaker.py
@@ -1,2 +1,34 @@
+from moviepy.editor import *
+from random import choice
+from os import listdir
+
+FONT_SIZE = 75
+
+
+# doesn't work, need to install "ImageMagick" or some shit like that and I don't know how
+def add_name_to_video(video_path: str, name: str):
+    clip = VideoFileClip(video_path)
+    text = TextClip(name, fontsize=FONT_SIZE, color="black")
+    text = text.set_position("center").set_duration(5)
+    video = CompositeVideoClip([clip, text])
+
+    return video
+
+
 class VideoMaker:
-    ...
+    def __init__(self, base_videos_path: str):
+        self.clips = []
+
+        for file_name in listdir(base_videos_path):
+            self.clips.append(VideoFileClip(f"{base_videos_path}/{file_name}"))
+
+    def make_video(self, video_path, name):
+        name_clip = VideoFileClip(video_path)
+        end_clip = add_name_to_video("./EquationSlayerBirthdayEnding.mp4", name)
+        final_video = concatenate_videoclips([name_clip, choice(self.clips), end_clip])
+        final_video.write_videofile(f"./{name}.mp4")
+
+
+if __name__ == "__main__":
+    video_maker = VideoMaker("../footage/base_videos")
+    video_maker.make_video("./name_clip_test.mp4", "sarah")
