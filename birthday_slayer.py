5555005a901c269d7747fa174e7c909a22b418b6
equationslayer12 <roy0708usa@gmail.com>
2023-08-17 13:52:18 +0300

    changed main.py and added base video example

--- birthday_slayer.py
+++ birthday_slayer.py
@@ -2,6 +2,7 @@ from videoMaker import videoMaker
 from thumbnailMaker import thumbnailMaker
 from os import listdir
 
+RAW_VID_DIR = "raw_videos/"
 
 def get_videos_in_folder(folder_path):
     video_paths = [f"{folder_path}/{video_name}" for video_name in listdir(folder_path)]
@@ -10,12 +11,15 @@ def get_videos_in_folder(folder_path):
 
 
 def main():
-    vid_maker = videoMaker.VideoMaker()
-    thumb_maker = thumbnailMaker.ThumbnailMaker()
+    vid_maker = videoMaker.VideoMaker("/footage/base_videos")
+    names = ["daniel", "equations"]
 
-    for video_name in ["daniel.mp4", "equations.mp4"]:
-        vid_maker.create_video(video_name)
-        thumb_maker.create_thumbnail(video_name)
+    with thumbnailMaker.ThumbnailMaker(r"/footage/base_thumbnail/base.jpg") as thumb_maker:
+        for name in names:
+            video_path = RAW_VID_DIR + name + ".mp4"
+            vid_maker.make_video(video_path, name)
+            vid_maker.make_short(video_path)
+            thumb_maker.create_thumbnail(video_path)
 
 
 if __name__ == "__main__":
