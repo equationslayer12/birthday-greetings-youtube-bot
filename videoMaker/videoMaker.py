41b79fda2eb4c186df04fda1df8143df316ca6f0
iKappyyyy <specialtimewithdaniel@gmail.com>
2023-08-19 01:07:34 +0300

    added shit to videomaker, mainly focusing on the random wishes part.

--- videoMaker/videoMaker.py
+++ videoMaker/videoMaker.py
@@ -1,35 +1,64 @@
 from moviepy.editor import *
-from random import choice
+from random import choice, shuffle
 from os import listdir
-
+from imageMaker import imageMaker
 
 FONT_SIZE = 75
 VIDEOS_PATH = "../birthdays_slayed/youtube_videos"
 SHORTS_PATH = "../birthdays_slayed/youtube_shorts"
+BASE_IMAGE_PATH = "../footage/base_images/base_end_of_video.png"
+ENDING_AUDIO_PATH = "../footage/part_3_audio.mp3"
+AMOUNT_OF_WISHES = 8
 
 
-# doesn't work, need to install "ImageMagick" or some shit like that and I don't know how
-def add_name_to_video(video_path: str, name: str):
-    clip = VideoFileClip(video_path)
-    text = TextClip(name, fontsize=FONT_SIZE, color="black")
-    text = text.set_position("center").set_duration(5)
-    video = CompositeVideoClip([clip, text])
+def create_image(name: str):
+    """ Returns path to end image """
+    with imageMaker.ImageMaker(BASE_IMAGE_PATH, "./", "../SecularOne.ttf", 150, -50) as image_maker:
+        image_maker.make_image(name)
 
-    return video
+    return f"./{name}.png"
+
+
+def create_end_clip(name: str) -> str:
+    """ Returns path to end clip """
+    ending_clip_audio = AudioFileClip(ENDING_AUDIO_PATH)
+    ending_clip = ImageClip(create_image(name)).set_duration(ending_clip_audio.duration)
+    ending_clip = ending_clip.set_audio(ending_clip_audio)
+    ending_clip.write_videofile("./ending_clip.mp4", fps=24)
+
+    return "./ending_clip.mp4"
+
+
+def birthday_wishes(every_base_video: list, every_base_video_to_include: list) -> list:
+    wishes = every_base_video.copy()
+    include_wishes = every_base_video_to_include.copy()
+    if (len(wishes) + len(include_wishes)) < AMOUNT_OF_WISHES:
+        print("Not enough base videos")
+        return []
+    shuffle(wishes)
+    wishes = wishes[:AMOUNT_OF_WISHES - len(include_wishes)]
+    wishes += include_wishes
+    shuffle(wishes)
+
+    return wishes
 
 
 class VideoMaker:
-    def __init__(self, base_videos_dir: str):
-        self.clips = []
-
-        for file_name in listdir(base_videos_dir):
-            self.clips.append(VideoFileClip(f"{base_videos_dir}/{file_name}"))
+    def __init__(self, base_videos_dir: str, base_videos_to_include_dir: str):
+        self.clips = [f"{base_videos_dir}/{file_name}" for file_name in listdir(base_videos_dir)]
+        self.include_clips = [f"{base_videos_to_include_dir}/{file_name}" for file_name in listdir(base_videos_to_include_dir)]
 
     def make_video(self, video_path, name) -> str:
         """ Returns path to the video """
         name_clip = VideoFileClip(video_path)
-        end_clip = add_name_to_video("./EquationSlayerBirthdayEnding.mp4", name)
-        final_video = concatenate_videoclips([name_clip, choice(self.clips), end_clip])
+        end_clip_path = create_end_clip(name)
+        end_clip = VideoFileClip(end_clip_path)
+        wishes = []
+        for path in birthday_wishes(self.clips, self.include_clips):
+            clip = VideoFileClip(path)
+            print(path, clip.duration)
+            wishes.append(clip)
+        final_video = concatenate_videoclips([name_clip] + wishes + [end_clip], method="compose")
         final_video.write_videofile(f"{VIDEOS_PATH}/{name}.mp4")
 
         return f"{VIDEOS_PATH}/{name}.mp4"
@@ -45,5 +74,5 @@ class VideoMaker:
 
 
 if __name__ == "__main__":
-    video_maker = VideoMaker("../footage/base_videos")
-    video_maker.make_short("./daniel.mp4")
+    video_maker = VideoMaker("../footage/base_videos", "../footage/base_videos_to_always_include")
+    video_maker.make_short(video_maker.make_video(r"D:\Downloads\the truth.mp4", "דניאל"), "דניאל")
